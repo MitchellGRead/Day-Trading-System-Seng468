7f7c7deed6142ef43c8e7ff7769975f52d7c8c01
MitchellGRead <mitchell.g.read@gmail.com>
2021-02-17 15:39:57 -0800

    Update config files for audit connections

--- WebService/docker_context/config.py
+++ WebService/docker_context/config.py
@@ -4,3 +4,7 @@ RUN_DEBUG = True
 WEB_SERVER_NAME = 'WebService'
 WEB_SERVER_IP = 'localhost'
 WEB_SERVER_PORT = 5000
+
+AUDIT_SERVER_NAME = 'AuditService'
+AUDIT_SERVER_IP = 'localhost'
+AUDIT_SERVER_PORT = 6500
