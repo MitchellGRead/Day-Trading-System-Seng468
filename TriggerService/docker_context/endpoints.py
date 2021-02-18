673bfd29ca57784372ba3f4980e99abfd1e9334e
MitchellGRead <mitchell.g.read@gmail.com>
2021-02-18 15:39:16 -0800

    add services endpoints, input schema, and api listeners

---
+++ TriggerService/docker_context/endpoints.py
@@ -0,0 +1,9 @@
+
+""" Post Endpoints """
+buy_amount_endpoint = '/set_buy/amount'
+buy_trigger_endpoint = '/set_buy/trigger'
+cancel_buy_trigger_endpoint = '/set_buy/cancel'
+
+sell_amount_endpoint = '/set_sell/amount'
+sell_trigger_endpoint = '/set_sell/trigger'
+cancel_sell_trigger_endpoint = '/set_sell/cancel'
---
+++ TriggerService/docker_context/triggerInputSchema.py
@@ -0,0 +1,60 @@
+from jsonschema import validate, ValidationError
+
+
+def validateRequest(data, schema):
+    if not data:
+        return False, 'Data is not Content-Type: application/json'
+
+    try:
+        validate(instance=data, schema=schema)
+        return True, ''
+    except ValidationError as err:
+        return False, str(err)
+
+
+def errorResult(err, data):
+    return {
+        'errorMessage': err,
+        'content': data
+    }
+
+
+one_to_three_letter_string = {
+    'type': 'string',
+    'minLength': 1,
+    'maxLength': 3
+}
+
+non_negative_number = {
+    'type': 'number',
+    'inclusiveMinimum': 0
+}
+
+base_transaction_schema = {
+    'type': 'object',
+    'properties': {
+        'transaction_num': {'type': 'integer'},
+        'user_id': {'type': 'string'},
+        'stock_symbol': one_to_three_letter_string,
+        'amount': non_negative_number
+    },
+    'required': ['transaction_num', 'user_id', 'stock_symbol', 'amount']
+}
+
+
+base_user_schema = {
+    'type': 'object',
+    'properties': {
+        'transaction_num': {'type': 'integer'},
+        'user_id': {'type': 'string'}
+    },
+    'required': ['transaction_num', 'user_id']
+}
+
+set_buy_amount_schema = base_transaction_schema
+set_buy_trigger_schema = base_transaction_schema
+cancel_buy_trigger_schema = base_user_schema
+
+set_sell_amount_schema = base_transaction_schema
+set_sell_trigger_schema = base_transaction_schema
+cancel_sell_trigger_schema = base_user_schema
