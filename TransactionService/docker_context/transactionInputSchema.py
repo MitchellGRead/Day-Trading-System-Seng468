146f90ac782354209e522f5194cefe2b3049044e
MitchellGRead <mitchell.g.read@gmail.com>
2021-02-18 16:04:37 -0800

    add api listeners, endpoints, and input schema for transaction service api

---
+++ TransactionService/docker_context/transactionInputSchema.py
@@ -0,0 +1,70 @@
+from jsonschema import validate, ValidationError
+
+
+def validateRequest(data, schema):
+    if not data:
+        return False, 'Data is not Content-Type: application/json'
+
+    try:
+        validate(instance=data, schema=schema)
+        return True, ''
+    except ValidationError as err:
+        return False, str(err)
+
+
+def errorResult(err, data):
+    return {
+        'errorMessage': err,
+        'content': data
+    }
+
+
+one_to_three_letter_string = {
+    'type': 'string',
+    'minLength': 1,
+    'maxLength': 3
+}
+
+non_negative_number = {
+    'type': 'number',
+    'inclusiveMinimum': 0
+}
+
+base_transaction_schema = {
+    'type': 'object',
+    'properties': {
+        'transaction_num': {'type': 'integer'},
+        'user_id': {'type': 'string'},
+        'stock_symbol': one_to_three_letter_string,
+        'amount': non_negative_number
+    },
+    'required': ['transaction_num', 'user_id', 'stock_symbol', 'amount']
+}
+
+
+base_user_schema = {
+    'type': 'object',
+    'properties': {
+        'transaction_num': {'type': 'integer'},
+        'user_id': {'type': 'string'}
+    },
+    'required': ['transaction_num', 'user_id']
+}
+
+quote_schema = {
+    'type': 'object',
+    'properties': {
+        'transaction_num': {'type': 'integer'},
+        'user_id': {'type': 'string'},
+        'stock_symbol': one_to_three_letter_string
+    },
+    'required': ['transaction_num', 'user_id', 'stock_symbol']
+}
+
+buy_schema = base_transaction_schema
+commit_buy_schema = base_transaction_schema
+cancel_buy_schema = base_user_schema
+
+sell_schema = base_transaction_schema
+commit_sell_schema = base_transaction_schema
+cancel_sell_schema = base_user_schema
