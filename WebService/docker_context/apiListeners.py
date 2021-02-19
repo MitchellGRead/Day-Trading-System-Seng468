48b01c5fd91fabb7d304591ada072d4c41b33790
MitchellGRead <mitchell.g.read@gmail.com>
2021-02-18 21:13:24 -0800

    add in audit handler listener for api and slight adjustments to json schemas

--- WebService/docker_context/apiListeners.py
+++ WebService/docker_context/apiListeners.py
@@ -1,9 +1,23 @@
 import aiohttp
+import config
+from sanic.log import logger
+from audit.AuditHandler import AuditHandler
 
 
 async def initClient(app, loop):
+    logger.debug('Starting http client')
     app.config['client'] = aiohttp.ClientSession(loop=loop)
 
 
 async def closeClient(app, loop):
     await app.config['client'].close()
+
+
+def initAudit(app, loop):
+    logger.debug('Creating audit handler')
+    app.config['audit'] = AuditHandler(
+        app.config['client'],
+        config.WEB_SERVER_NAME,
+        config.AUDIT_SERVER_IP,
+        config.AUDIT_SERVER_PORT
+    )
