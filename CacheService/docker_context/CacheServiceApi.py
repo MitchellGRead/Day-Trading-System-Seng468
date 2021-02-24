57cdd71a9ffa54c0d78232e46d536dcaf877fac0
Mitch Petersen <mitchpetersen1@gmail.com>
2021-02-23 19:48:54 -0800

    Latest version of Cache API

---
+++ CacheService/docker_context/CacheServiceApi.py
@@ -0,0 +1,188 @@
+from sanic import Sanic, response
+
+import config
+import endpoints
+import apiListeners
+from cacheInputSchema import *
+
+app = Sanic(config.CACHE_SERVER_NAME)
+
+
+# GET ENDPOINTS -----------------------------------------------
+
+# Get the user's funds
+@app.route(endpoints.get_user_funds_endpoint, methods=['GET'])
+async def getUserFunds(request, user_id):
+    result = await app.config['serviceLogic'].getUserFunds(user_id)
+    return result
+
+
+# Get the user's stocks
+@app.route(endpoints.get_user_stocks_endpoint, methods=['GET'])
+async def getUserStocks(request, user_id, stock_id):
+    if stock_id:
+        res, err = validateRequest(stock_id, one_to_three_letter_string)
+        if not res:
+            return response.json(errorResult(err, stock_id), status=400)
+
+    result = await app.config['serviceLogic'].getUserStocks(user_id, stock_id)
+    return result
+
+
+# Gets a quote price
+@app.route(endpoints.get_quote_endpoint, methods=['GET'])
+async def getQuote(request, user_id, stock_id, trans_num):
+    if stock_id:
+        res, err = validateRequest(stock_id, one_to_three_letter_string)
+        if not res:
+            return response.json(errorResult(err, stock_id), status=400)
+
+    result = await app.config['serviceLogic'].getQuote(trans_num, user_id, stock_id)
+    return result
+
+
+# Get active buy command
+@app.route(endpoints.get_buy_endpoint, methods=['GET'])
+async def getBuyStocks(request, user_id):
+    if user_id:
+        res, err = validateRequest(user_id, is_user_id)
+        if not res:
+            return response.json(errorResult(err, user_id), status=400)
+
+    result = await app.config['serviceLogic'].getBuyStocks(user_id)
+    return result
+
+
+# Get active sell command
+@app.route(endpoints.get_sell_endpoint, methods=['GET'])
+async def getSellStocks(request, user_id):
+    if user_id:
+        res, err = validateRequest(user_id, is_user_id)
+        if not res:
+            return response.json(errorResult(err, user_id), status=400)
+
+    result = await app.config['serviceLogic'].getSellStocks(user_id)
+    return result
+
+
+# POST ENDPOINTS -----------------------------------------------
+
+
+# Add funds to user's account
+@app.route(endpoints.add_funds_endpoint, methods=['POST'])
+async def addFunds(request):
+    res, err = validateRequest(request.json, add_funds_schema)
+    if not res:
+        return response.json(errorResult(err, request.json), status=400)
+
+    data = request.json
+    result = await app.config['serviceLogic'].addFunds(data['user_id'], data['funds'])
+    return result
+
+
+# Remove funds from user's account
+# @app.route(endpoints.remove_funds_endpoint, methods=['POST'])
+async def removeFunds(request):
+    res, err = validateRequest(request.json, remove_funds_schema)
+    if not res:
+        return response.json(errorResult(err, request.json), status=400)
+
+    # TODO: Decide if this method should be supported
+    return None
+
+
+# Buy stocks endpoint -- removes funds and adds stocks
+@app.route(endpoints.buy_stocks_endpoint, methods=['POST'])
+async def buyStocks(request):
+    res, err = validateRequest(request.json, buy_stock_schema)
+    if not res:
+        return response.json(errorResult(err, request.json), status=400)
+
+    data = request.json
+    result = await app.config['serviceLogic'].buyStocks(data['user_id'], data['stock_symbol'],
+                                                                     data['stock_amount'], data['funds'])
+    return result
+
+
+# Sell stocks endpoint -- add funds and removes stocks
+@app.route(endpoints.sell_stocks_endpoint, methods=['POST'])
+async def sellStocks(request):
+    res, err = validateRequest(request.json, sell_stock_schema)
+    if not res:
+        return response.json(errorResult(err, request.json), status=400)
+
+    data = request.json
+    result = await app.config['serviceLogic'].sellStocks(data['user_id'], data['stock_symbol'],
+                                                         data['stock_amount'], data['funds'])
+    return result
+
+
+# Get active buy command
+@app.route(endpoints.commit_buy_endpoint, methods=['POST'])
+async def commitBuyStocks(request):
+    res, err = validateRequest(request.json, user_schema)
+    if not res:
+        return response.json(errorResult(err, request.json), status=400)
+
+    data = request.json
+    result = await app.config['serviceLogic'].commitBuyStocks(data['user_id'])
+    return result
+
+
+# Get active buy command
+@app.route(endpoints.commit_sell_endpoint, methods=['POST'])
+async def commitSellStocks(request):
+    res, err = validateRequest(request.json, user_schema)
+    if not res:
+        return response.json(errorResult(err, request.json), status=400)
+
+    data = request.json
+    result = await app.config['serviceLogic'].commitSellStocks(data['user_id'])
+    return result
+
+
+# Get active buy command
+@app.route(endpoints.cancel_buy_endpoint, methods=['POST'])
+async def cancelBuy(request):
+    res, err = validateRequest(request.json, user_schema)
+    if not res:
+        return response.json(errorResult(err, request.json), status=400)
+
+    data = request.json
+    result = await app.config['serviceLogic'].cancelBuy(data['user_id'])
+    return result
+
+
+# Get active buy command
+@app.route(endpoints.cancel_sell_endpoint, methods=['POST'])
+async def cancelSell(request):
+    res, err = validateRequest(request.json, user_schema)
+    if not res:
+        return response.json(errorResult(err, request.json), status=400)
+
+    data = request.json
+    result = await app.config['serviceLogic'].cancelSell(data['user_id'])
+    return result
+
+
+# DB SERVICE INITIALIZATION -----------------------------------------------
+
+
+if __name__ == "__main__":
+    # Before app start
+    app.register_listener(apiListeners.initClient, 'before_server_start')
+    app.register_listener(apiListeners.connectRedis, 'before_server_start')
+    app.register_listener(apiListeners.initAudit, 'before_server_start')
+    app.register_listener(apiListeners.initLegacyStock, 'before_server_start')
+    app.register_listener(apiListeners.initCacheLogic, 'before_server_start')
+    app.register_listener(apiListeners.initServiceLogic, 'before_server_start')
+    # Before app stop
+    app.register_listener(apiListeners.closeClient, 'before_server_stop')
+    app.register_listener(apiListeners.closeRedis, 'before_server_stop')
+
+    app.run(
+        host=config.CACHE_SERVER_IP,
+        port=config.CACHE_SERVER_PORT,
+        debug=config.RUN_DEBUG,
+        auto_reload=True
+    )
---
+++ CacheService/docker_context/apiListeners.py
@@ -0,0 +1,67 @@
+import aiohttp
+import aioredis
+import config
+from sanic.log import logger
+from handlers.AuditHandler import AuditHandler
+from handlers.RedisHandler import RedisHandler
+from handlers.CacheHandler import CacheHandler
+from handlers.LegacyStockServerHandler import LegacyStockServerHandler
+from ServiceLogic import ServiceLogic
+
+
+async def initClient(app, loop):
+    app.config['client'] = aiohttp.ClientSession(loop=loop)
+
+
+async def closeClient(app, loop):
+    await app.config['client'].close()
+
+
+async def connectRedis(app, loop):
+    app.config['redisPool'] = await aioredis.create_pool(
+        (config.CACHE_REDIS_IP, config.CACHE_REDIS_PORT),
+        minsize=5,
+        maxsize=10,
+        loop=loop
+    )
+    await app.config['redisPool'].commands.ServerCommandsMixin.config_set(parameter='maxmemory', value='2gb')
+    await app.config['redisPool'].commands.ServerCommandsMixin.config_set(parameter='maxmemory-policy',
+                                                                          value='allkeys-lru')
+
+
+async def initRedisHandler(app, loop):
+    app.config['redisHandler'] = RedisHandler(app.config['redisPool'], app.config['client'],
+                                              config.DATABASE_SERVER_IP,
+                                              config.DATABASE_SERVER_PORT)
+
+
+async def initAudit(app, loop):
+    logger.debug('Creating audit handler')
+    app.config['audit'] = AuditHandler(
+        app.config['client'],
+        config.CACHE_SERVER_NAME,
+        config.AUDIT_SERVER_IP,
+        config.AUDIT_SERVER_PORT
+    )
+
+
+async def initLegacyStock(app, loop):
+    app.config['legacyStock'] = LegacyStockServerHandler(config.LEGACY_STOCK_SERVER_IP, config.LEGACY_STOCK_SERVER_PORT,
+                                                         app.config['audit'])
+
+
+async def initCacheLogic(app, loop):
+    app.config['cacheLogic'] = CacheHandler(app.config['redisPool'],
+                                            app.config['redisHandler'], app.config['audit'],
+                                            app.config['client'], config.DATABASE_SERVER_IP,
+                                            config.DATABASE_SERVER_PORT, app.config['legacyStock'])
+
+
+async def initServiceLogic(app, loop):
+    app.config['serviceLogic'] = ServiceLogic(app.config['cacheLogic'])
+
+
+async def closeRedis(app, loop):
+    redis_pool = app.config['redisPool']
+    redis_pool.close()
+    await redis_pool.await_closed()
