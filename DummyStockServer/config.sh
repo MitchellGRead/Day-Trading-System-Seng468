39eb2d67b8002b533b4e6401b6546d8ac9e8f158
Anmol Monga <a.monga@outlook.com>
2021-02-27 15:41:38 -0800

    Containerize dummy stock service.

--- DummyStockServer/config.sh
+++ DummyStockServer/config.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+network_name=myNetwork
+
+audit_base_name=audit
+audit_base_num=1
+
+audit_service_port=6500
+
+audit_service_image=$audit_base_name:$audit_base_num
+audit_service_name=$audit_base_name-$audit_base_num
+
+db_base_name=dbmgr
+db_base_num=1
+
+db_service_port=5656
+
+db_service_image=$db_base_name:$db_base_num
+db_service_name=$db_base_name-$db_base_num
+
 dummy_base_name=dummy-stock
 dummy_base_num=1
 
--- config.sh
+++ config.sh
@@ -17,3 +17,11 @@ db_service_port=5656
 
 db_service_image=$db_base_name:$db_base_num
 db_service_name=$db_base_name-$db_base_num
+
+dummy_base_name=dummy-stock
+dummy_base_num=1
+
+dummy_service_port=4444
+
+dummy_service_image=$dummy_base_name:$dummy_base_num
+dummy_service_name=$dummy_base_name-$dummy_base_num
