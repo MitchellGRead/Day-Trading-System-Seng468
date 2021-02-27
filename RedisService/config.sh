5915de474f9bf67c8d3e64713a1bfac71f6e5621
Anmol Monga <a.monga@outlook.com>
2021-02-27 15:46:27 -0800

    Containerize redis service.

--- RedisService/config.sh
+++ RedisService/config.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+network_name=myNetwork
+
+audit_base_name=audit
+audit_base_num=1
+
+audit_service_port=6500
+
+audit_service_image=$audit_base_name:$audit_base_num
+audit_service_name=$audit_base_name-$audit_base_num
+
+db_base_name=dbmgr
+db_base_num=1
+
+db_service_port=5656
+
+db_service_image=$db_base_name:$db_base_num
+db_service_name=$db_base_name-$db_base_num
+
+dummy_base_name=dummy-stock
+dummy_base_num=1
+
+dummy_service_port=4444
+
+dummy_service_image=$dummy_base_name:$dummy_base_num
+dummy_service_name=$dummy_base_name-$dummy_base_num
+
 redis_base_name=redis
 redis_base_num=1
 
