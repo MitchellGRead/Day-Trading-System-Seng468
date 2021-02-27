cdcbd4c1d2b6d41b8b978386502db9b08b829a9b
Anmol Monga <a.monga@outlook.com>
2021-02-27 14:09:29 -0800

    Containerize audit service.

---
+++ deploy_config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curr_path=$(dirname $0)
+
+cp $curr_path/config.py $curr_path/AuditService/docker_context
+cp $curr_path/config.sh $curr_path/AuditService/config.sh
+
+# cp $curr_path/config.py $curr_path/AuditService/docker_context
+# cp $curr_path/config.sh $curr_path/AuditService/config.sh
+
+# cp $curr_path/config.py $curr_path/AuditService/docker_context
+# cp $curr_path/config.sh $curr_path/AuditService/config.sh
+
+# cp $curr_path/config.py $curr_path/AuditService/docker_context
+# cp $curr_path/config.sh $curr_path/AuditService/config.sh
+
+# cp $curr_path/config.py $curr_path/AuditService/docker_context
+# cp $curr_path/config.sh $curr_path/AuditService/config.sh
