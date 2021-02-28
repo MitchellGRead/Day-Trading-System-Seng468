20f74c479fa74992a8e65ebd5d8f6a4329f95c97
Anmol Monga <a.monga@outlook.com>
2021-02-27 17:30:23 -0800

    Fix generator shell scripts.

--- WorkloadGenerator/build_service.sh
+++ WorkloadGenerator/build_service.sh
@@ -5,6 +5,6 @@ source $curr_path/config.sh
 
 $curr_path/stop_service.sh 2>/dev/null
 
-echo 'Building web service...'
-image=$web_service_image
+echo 'Building generator...'
+image=$generator_image
 sudo docker build -f $curr_path/Dockerfile -t $image $curr_path/docker_context/ > /dev/null
