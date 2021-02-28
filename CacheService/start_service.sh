7a89ccae953ed2e271cd860d16b54dead77ff8ec
Anmol Monga <a.monga@outlook.com>
2021-02-27 16:02:19 -0800

    Containerize cache service.

---
+++ CacheService/start_service.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curr_path=$(dirname $0)
+source $curr_path/config.sh
+
+image=$cache_service_image
+cont=$cache_service_name
+port=$cache_service_port
+network=$network_name
+
+$curr_path/build_service.sh 2>/dev/null
+echo 'Starting cache service...'
+sudo docker rm $cont >/dev/null 2>&1
+sudo docker run --name $cont -p $port:$port --network $network -d $image > /dev/null
