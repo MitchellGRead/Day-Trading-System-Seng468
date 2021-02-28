134f4351eabbf4419d2ff46d0afb14af9784f0b1
Anmol Monga <a.monga@outlook.com>
2021-02-27 17:18:08 -0800

    Containerize worjload generator.

--- WebService/config.sh
+++ WebService/config.sh
@@ -65,3 +65,11 @@ web_service_port=5000
 
 web_service_image=$web_base_name:$web_base_num
 web_service_name=$web_base_name-$web_base_num
+
+generator_base_name=gen
+generator_base_num=1
+
+generator_port=5555
+
+generator_image=$generator_base_name:$generator_base_num
+generator_name=$generator_base_name-$generator_base_num
