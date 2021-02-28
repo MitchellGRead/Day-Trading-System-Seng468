eea4e28fb6cf5b190f75f254052ea32bb3d93631
Anmol Monga <a.monga@outlook.com>
2021-02-27 16:29:31 -0800

    Containerize web service.

--- config.sh
+++ config.sh
@@ -57,3 +57,11 @@ trigger_service_port=7000
 
 trigger_service_image=$trigger_base_name:$trigger_base_num
 trigger_service_name=$trigger_base_name-$trigger_base_num
+
+web_base_name=web
+web_base_num=1
+
+web_service_port=5000
+
+web_service_image=$web_base_name:$web_base_num
+web_service_name=$web_base_name-$web_base_num
