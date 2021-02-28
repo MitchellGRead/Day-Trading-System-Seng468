c8273f28f8d87105a40ec2c95a72472497126570
Anmol Monga <a.monga@outlook.com>
2021-02-27 16:15:42 -0800

    Containerize transaction service.

--- DatabaseService/config.sh
+++ DatabaseService/config.sh
@@ -40,4 +40,12 @@ cache_base_num=1
 cache_service_port=9999
 
 cache_service_image=$cache_base_name:$cache_base_num
-cache_service_name=$cache_base_name-$cache_base_num
+cache_service_name=$cache_base_name-$cache_base_numaudit_service_image
+
+trans_base_name=trsrvr
+trans_base_num=1
+
+trans_service_port=6666
+
+trans_service_image=$trans_base_name:$trans_base_num
+trans_service_name=$trans_base_name-$trans_base_num
