8ae0e16e5dce621c9029c51cdfc542f5b42859fd
Mitch Petersen <mitchpetersen1@gmail.com>
2021-03-15 14:04:22 -0700

    Added checking of running quote hits, to stop repeats running concurrently.

--- CacheService/docker_context/LegacyStockServerHandler.py
+++ CacheService/docker_context/LegacyStockServerHandler.py
@@ -1,14 +1,22 @@
 import asyncio
+from time import time
 
 from sanic.log import logger
 
 
-class LegacyStockServerHandler:
+def currentTime():
+    return time()
 
-    def __init__(self, ip, port, audit):
+
+class LegacyStockServerHandler:
+    _QUOTE_CACHE_TIME_LIMIT_SEC = 10
+
+    def __init__(self, ip, port, audit, redisHandler):
         self.ip = ip
         self.port = port
         self.audit = audit
+        self.RedisHandler = redisHandler
+        self.tracker = {}
 
     async def quoteSocket(self):
         logger.info(f"{__name__} - Attempting Connection to Quote Server")
@@ -17,6 +25,26 @@ class LegacyStockServerHandler:
         return reader, writer
 
     async def getQuote(self, trans_num, user_id, stock_symbol):
+        if stock_symbol in self.tracker.keys():
+            await self.tracker[stock_symbol]
+            check = await self.RedisHandler.rExists(stock_symbol)
+            if check:
+                quote = await self.RedisHandler.rGet(stock_symbol)
+                return quote['price'], 200
+            else:
+                return await self.newQuoteTask(trans_num, user_id, stock_symbol)
+        else:
+            return await self.newQuoteTask(trans_num, user_id, stock_symbol)
+
+    async def newQuoteTask(self, trans_num, user_id, stock_symbol):
+        newTask = asyncio.create_task(self._getQuoteFunc(trans_num, user_id, stock_symbol))
+        self.tracker[stock_symbol] = newTask
+        await newTask
+
+        quote = await self.RedisHandler.rGet(stock_symbol)
+        return quote['price'], 200
+
+    async def _getQuoteFunc(self, trans_num, user_id, stock_symbol):
         reader, writer = await self.quoteSocket()
 
         message = f'{stock_symbol},{user_id}\n'
@@ -34,5 +62,11 @@ class LegacyStockServerHandler:
         quote_time = int(quote_time)
 
         await self.audit.handleQuote(trans_num, user_id, stock_symbol, price, quote_time, cryptokey)
-        # DO FAILURE
-        return {'price': price}, 200
+        # TODO: Failure.
+
+        await self.RedisHandler.rSet(stock_symbol, {'stock_id': stock_symbol, 'price': price, 'time': currentTime()})
+        del self.tracker[stock_symbol]
+
+        return
+
+
