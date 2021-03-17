3bcab4cb9cbc38616828249985cdc4c8e0a3b2e3
MitchellGRead <mitchell.g.read@gmail.com>
2021-03-17 16:34:27 -0700

    implement adding the funds UI and generalize the error message

--- client-user/src/App.js
+++ client-user/src/App.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Quote from './components/Quote';
 import UserCredentials from './components/UserCredentials';
+import Transaction from './components/Transaction';
+import Add from './components/Add';
+import Error from './components/Error';
 import './styles/App.css';
 
 const App = (props) => {
   const [userId, setUserId] = useState('');
   const [isAdmin, setIsAdmin] = useState(false);
+  const [error, setError] = useState('');
 
   console.log(userId, isAdmin)
 
@@ -21,8 +25,12 @@ const App = (props) => {
           onChangeIsAdmin={setIsAdmin}
         />
       </div>
+      <Error error={error} />
+      <Quote userId={userId} onError={setError} />
 
-      <Quote userId={userId} />
+      <div className='main-content'>
+        <Add userId={userId} onError={setError} />
+      </div>
 
     </div>
   );
--- client-user/src/api.js
+++ client-user/src/api.js
@@ -4,7 +4,17 @@ const API_URL = 'https://localhost:5000';
 
 // TODO get this actually hooked up
 export async function getQuote(userId, stockSymbol, transNum) {
-  // let endpoint = `/get/QUOTE/trans/${transNum}/user_id/${userId}/stock_symbol/${stockSymbol}`;
-  // return await axios.get(`${API_URL}${endpoint}`)
-  return 30.2;
+  let endpoint = `/get/QUOTE/trans/${transNum}/user_id/${userId}/stock_symbol/${stockSymbol}`;
+  return await axios.get(`${API_URL}${endpoint}`)
+}
+
+export async function postFunds(userId, funds) {
+  let endpoint = `/add`;
+  let data = {
+    'user_id': userId,
+    'amount': funds,
+    'command': 'ADD',
+    'transaction_num': 3
+  };
+  return await axios.post(`${API_URL}${endpoint}`, data);
 }
