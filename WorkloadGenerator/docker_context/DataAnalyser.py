7830db254ebfc044f0dfab12ea6b1cc059eb1dec
MitchellGRead <mitchell.g.read@gmail.com>
2021-03-22 11:28:44 -0700

    Create an analyser to save time data and execution results

--- WorkloadGenerator/docker_context/DataAnalyser.py
+++ WorkloadGenerator/docker_context/DataAnalyser.py
@@ -1,23 +1,59 @@
+from datetime import datetime
+from pathlib import Path
+
 import pandas as pd
+
 from eventLogger import logger
 
 
-class Grapher:
+class DataAnalyser:
 
     def __init__(
             self,
-            num_users,
-            num_commands,
-            execution_time,
-            command_times
+            num_users,  # int
+            num_commands,  # int
+            execution_time,  # float
+            command_times  # dict
     ):
         self.num_users = num_users
         self.num_commands = num_commands
         self.execution_time = execution_time
-        self.command_times = command_times
+        self.command_times = self._toDataFrame(command_times)
         self.cps = self.num_commands / self.execution_time  # Commands per second
+        self._SAVE_PATH = f'./commandanalysis/{num_users}_users_{self._currentDate()}/'
 
-    def logSummary(self):
+    def _toDataFrame(self, data):
+        return pd.DataFrame(data=dict([(k, pd.Series(v)) for k, v in data.items()]))
+
+    def _makePath(self, pathToMake):
+        path = Path(pathToMake)
+        path.mkdir(parents=True, exist_ok=True)
+
+    def _currentDate(self):
+        return datetime.now().strftime('%m-%d-%Y_%H-%M-%S')
+
+    def saveCommandTimes(self):
+        self._makePath(self._SAVE_PATH)
+
+        save_path = f'{self._SAVE_PATH}command_times.csv'
+        self.command_times.to_csv(save_path, index=False)
+
+    def logSummary(self, save=True):
         logger.info(f'Total commands executed - {self.num_commands}')
-        logger.info(f'Total execution time - {self.execution_time}s')
+        logger.info(f'Total execution time (s) - {self.execution_time}')
         logger.info(f'Commands per second - {self.cps}')
+        logger.info(self.command_times.describe())
+
+        if save:
+            self.saveSummary()
+
+    def saveSummary(self):
+        self._makePath(self._SAVE_PATH)
+        save_path = f'{self._SAVE_PATH}summary.csv'
+
+        self.command_times.describe().to_csv(save_path)
+        with open(save_path, 'a') as file:
+            file.write(f'Total commands executed - {self.num_commands}\n')
+            file.write(f'Total execution time (s) - {self.execution_time}\n')
+            file.write(f'Commands per second - {self.cps}\n')
+
