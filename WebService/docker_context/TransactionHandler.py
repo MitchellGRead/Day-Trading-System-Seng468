e8effe8156432af1d7377893da4d493b8fcee4b8
MitchellGRead <mitchell.g.read@gmail.com>
2021-03-24 16:05:54 -0700

    update the transaction handler in web api to return the resp and status

--- WebService/docker_context/TransactionHandler.py
+++ WebService/docker_context/TransactionHandler.py
@@ -9,33 +9,33 @@ class TransactionHandler:
 
     async def handleQuote(self, data):
         endpoint = f'/get/{data["command"]}/trans/{data["transaction_num"]}/user/{data["user_id"]}/stock/{data["stock_symbol"]}'
-        resp = await self.client.getRequest(f'{self.url}{endpoint}')
-        return resp
+        resp, status = await self.client.getRequest(f'{self.url}{endpoint}')
+        return resp, status
 
     async def handleAdd(self, data):
-        resp = await self.client.postRequest(f'{self.url}/add', data)
-        return resp
+        resp, status = await self.client.postRequest(f'{self.url}/add', data)
+        return resp, status
 
     async def handleBuy(self, data):
-        resp = await self.client.postRequest(f'{self.url}/buy', data)
-        return resp
+        resp, status = await self.client.postRequest(f'{self.url}/buy', data)
+        return resp, status
 
     async def handleCommitBuy(self, data):
-        resp = await self.client.postRequest(f'{self.url}/buy/commit', data)
-        return resp
+        resp, status = await self.client.postRequest(f'{self.url}/buy/commit', data)
+        return resp, status
 
     async def handleCancelBuy(self, data):
-        resp = await self.client.postRequest(f'{self.url}/buy/cancel', data)
-        return resp
+        resp, status = await self.client.postRequest(f'{self.url}/buy/cancel', data)
+        return resp, status
 
     async def handleSell(self, data):
-        resp = await self.client.postRequest(f'{self.url}/sell', data)
-        return resp
+        resp, status = await self.client.postRequest(f'{self.url}/sell', data)
+        return resp, status
 
     async def handleCommitSell(self, data):
-        resp = await self.client.postRequest(f'{self.url}/sell/commit', data)
-        return resp
+        resp, status = await self.client.postRequest(f'{self.url}/sell/commit', data)
+        return resp, status
 
     async def handleCancelSell(self, data):
-        resp = await self.client.postRequest(f'{self.url}/sell/cancel', data)
-        return resp
+        resp, status = await self.client.postRequest(f'{self.url}/sell/cancel', data)
+        return resp, status
