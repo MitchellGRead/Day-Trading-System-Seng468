301d08582270e84d9e3935700ced1bfae7beff98
MitchellGRead <mitchell.g.read@gmail.com>
2021-03-24 16:06:28 -0700

    update the web api to handle CORS headers for resp and request and send the resp and status for the transaction, quote, and add commands

--- WebService/docker_context/WebServiceApi.py
+++ WebService/docker_context/WebServiceApi.py
@@ -1,5 +1,6 @@
 
 from sanic import Sanic, response
+from sanic.log import logger
 
 import config
 import endpoints
@@ -22,8 +23,8 @@ async def getQuote(request, command, trans_num, user_id, stock_symbol):
     if not res:
         return response.json(errorResult(err, data), status=400)
 
-    await app.config['logic'].handleQuote(data)
-    return response.json(data)
+    resp, status = await app.config['logic'].handleQuote(data)
+    return response.json(resp, status=status)
 
 
 @app.route(endpoints.display_summary_endpoint, methods=['GET'])
@@ -60,55 +61,55 @@ async def createDumplog(request, command, trans_num, filename):
     return response.json(data)
 
 
-@app.route(endpoints.add_funds_endpoint, methods=['POST'])
+@app.route(endpoints.add_funds_endpoint, methods=['POST', 'OPTIONS'])
 async def addFunds(request):
     res, err = validateRequest(request.json, add_schema)
     if not res:
         return response.json(errorResult(err, request.json), status=400)
 
     data = request.json
-    await app.config['logic'].handleAdd(data)
-    return response.json(data)
+    resp, status = await app.config['logic'].handleAdd(data)
+    return response.json(resp, status=status)
 
 
 # BUY ENDPOINTS ------------------------------------------------
-@app.route(endpoints.buy_endpoint, methods=['POST'])
+@app.route(endpoints.buy_endpoint, methods=['POST', 'OPTIONS'])
 async def buyStock(request):
     res, err = validateRequest(request.json, buy_schema)
     if not res:
         return response.json(errorResult(err, request.json), status=400)
 
     data = request.json
-    await app.config['logic'].handleBuy(data)
-    return response.json(data)
+    resp, status = await app.config['logic'].handleBuy(data)
+    return response.json(resp, status=status)
 
 
-@app.route(endpoints.commit_buy_endpoint, methods=['POST'])
+@app.route(endpoints.commit_buy_endpoint, methods=['POST', 'OPTIONS'])
 async def commitBuy(request):
     res, err = validateRequest(request.json, commit_buy_schema)
     if not res:
         return response.json(errorResult(err, request.json), status=400)
 
     data = request.json
-    await app.config['logic'].handleCommitBuy(data)
-    return response.json(data)
+    resp, status = await app.config['logic'].handleCommitBuy(data)
+    return response.json(resp, status=status)
 
 
-@app.route(endpoints.cancel_buy_endpoint, methods=['POST'])
+@app.route(endpoints.cancel_buy_endpoint, methods=['POST', 'OPTIONS'])
 async def cancelBuy(request):
     res, err = validateRequest(request.json, cancel_buy_schema)
     if not res:
         return response.json(errorResult(err, request.json), status=400)
 
     data = request.json
-    await app.config['logic'].handleCancelBuy(data)
-    return response.json(data)
+    resp, status = await app.config['logic'].handleCancelBuy(data)
+    return response.json(resp, status=status)
 
 # --------------------------------------------------------------
 
 
 # BUY TRIGGER ENDPOINTS ----------------------------------------
-@app.route(endpoints.set_buy_amount_endpoint, methods=['POST'])
+@app.route(endpoints.set_buy_amount_endpoint, methods=['POST', 'OPTIONS'])
 async def setBuyAmount(request):
     res, err = validateRequest(request.json, set_buy_amount_schema)
     if not res:
@@ -119,7 +120,7 @@ async def setBuyAmount(request):
     return response.json(data)
 
 
-@app.route(endpoints.set_buy_trigger_endpoint, methods=['POST'])
+@app.route(endpoints.set_buy_trigger_endpoint, methods=['POST', 'OPTIONS'])
 async def setBuyTrigger(request):
     res, err = validateRequest(request.json, set_buy_trigger_schema)
     if not res:
@@ -130,7 +131,7 @@ async def setBuyTrigger(request):
     return response.json(data)
 
 
-@app.route(endpoints.cancel_set_buy_endpoint, methods=['POST'])
+@app.route(endpoints.cancel_set_buy_endpoint, methods=['POST', 'OPTIONS'])
 async def cancelBuyTrigger(request):
     res, err = validateRequest(request.json, cancel_set_buy_schema)
     if not res:
@@ -143,43 +144,43 @@ async def cancelBuyTrigger(request):
 
 
 # SELL ENDPOINTS -----------------------------------------------
-@app.route(endpoints.sell_endpoint, methods=['POST'])
+@app.route(endpoints.sell_endpoint, methods=['POST', 'OPTIONS'])
 async def sellStock(request):
     res, err = validateRequest(request.json, sell_schema)
     if not res:
         return response.json(errorResult(err, request.json), status=400)
 
     data = request.json
-    await app.config['logic'].handleSell(data)
-    return response.json(data)
+    resp, status = await app.config['logic'].handleSell(data)
+    return response.json(resp, status=status)
 
 
-@app.route(endpoints.commit_sell_endpoint, methods=['POST'])
+@app.route(endpoints.commit_sell_endpoint, methods=['POST', 'OPTIONS'])
 async def commitSell(request):
     res, err = validateRequest(request.json, commit_sell_schema)
     if not res:
         return response.json(errorResult(err, request.json), status=400)
 
     data = request.json
-    await app.config['logic'].handleCommitSell(data)
-    return response.json(data)
+    resp, status = await app.config['logic'].handleCommitSell(data)
+    return response.json(resp, status=status)
 
 
-@app.route(endpoints.cancel_sell_endpoint, methods=['POST'])
+@app.route(endpoints.cancel_sell_endpoint, methods=['POST', 'OPTIONS'])
 async def cancelSell(request):
     res, err = validateRequest(request.json, cancel_sell_schema)
     if not res:
         return response.json(errorResult(err, request.json), status=400)
 
     data = request.json
-    await app.config['logic'].handleCancelSell(data)
-    return response.json(data)
+    resp, status = await app.config['logic'].handleCancelSell(data)
+    return response.json(resp, status=status)
 
 # --------------------------------------------------------------
 
 
 # SELL TRIGGER ENDPOINTS ---------------------------------------
-@app.route(endpoints.set_sell_amount_endpoint, methods=['POST'])
+@app.route(endpoints.set_sell_amount_endpoint, methods=['POST', 'OPTIONS'])
 async def setSellAmount(request):
     res, err = validateRequest(request.json, set_sell_amount_schema)
     if not res:
@@ -190,7 +191,7 @@ async def setSellAmount(request):
     return response.json(data)
 
 
-@app.route(endpoints.set_sell_trigger_endpoint, methods=['POST'])
+@app.route(endpoints.set_sell_trigger_endpoint, methods=['POST', 'OPTIONS'])
 async def setSellTrigger(request):
     res, err = validateRequest(request.json, set_sell_trigger_schema)
     if not res:
@@ -201,7 +202,7 @@ async def setSellTrigger(request):
     return response.json(data)
 
 
-@app.route(endpoints.cancel_set_sell_endpoint, methods=['POST'])
+@app.route(endpoints.cancel_set_sell_endpoint, methods=['POST', 'OPTIONS'])
 async def cancelSellTrigger(request):
     res, err = validateRequest(request.json, cancel_set_sell_schema)
     if not res:
@@ -211,6 +212,20 @@ async def cancelSellTrigger(request):
     await app.config['logic'].handleCancelSellTrigger(data)
     return response.json(data)
 # --------------------------------------------------------------
+
+@app.middleware('response')
+async def enableResponseCORS(request, response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+@app.middleware('request')
+async def allowCORSOptions(request):
+    cors_headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST',
+        'Access-Control-Allow-Headers': 'Content-Type'
+    }
+    if request.method == 'OPTIONS':
+        return response.json({}, headers=cors_headers)
 
 
 if __name__ == '__main__':
