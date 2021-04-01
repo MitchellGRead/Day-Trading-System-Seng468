b83b8c86bdc9d8492bdb6a51283d6769c92c3f02
Mitch Petersen <mitchpetersen1@gmail.com>
2021-03-31 17:41:20 -0700

    Restrict cores (attempt 2)

--- CacheService/start_service.sh
+++ CacheService/start_service.sh
@@ -11,4 +11,4 @@ network=$network_name
 $curr_path/build_service.sh 2>/dev/null
 echo 'Starting cache service...'
 sudo docker rm $cont >/dev/null 2>&1
-sudo docker run --name $cont -p $port:$port --network $network -d $image > /dev/null --cpuset-cpus="0"
+sudo docker run --name $cont -p $port:$port --cpuset-cpus="0" --network $network -d $image > /dev/null
